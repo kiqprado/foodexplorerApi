a0d079ac13718a4f0f791456a8ec0455971a39e6
Kaique Prado <kiqprado@outlook.com>
2024-04-30 04:10:10 -0300

    feat: knex config initial

--- src/database/knex/migrations/20240430060341_createDish.js
+++ src/database/knex/migrations/20240430060341_createDish.js
@@ -1,11 +1,12 @@
-exports.up = knex.schema.createTable('dish', table => {
-  table.increments('id')
-  table.text('photo').default(null)
-  table.text('title').notNullable()
-  table.text('description')
-  table.decimal('price', 8, 2)
-  
-  table.integer('category_id').references('id').inTable('category')
-})
+exports.up = knex =>
+  knex.schema.createTable('dish', table => {
+    table.increments('id')
+    table.text('photo').default(null)
+    table.text('title').notNullable()
+    table.text('description')
+    table.decimal('price', 8, 2)
 
-exports.down = knex.schema.dropTable('dish')
+    table.integer('category_id').references('id').inTable('category')
+  })
+
+exports.down = knex => knex.schema.dropTable('dish')
